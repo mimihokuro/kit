4eaf417e45478676c100ec1c699f0be474ef9aa1
mimihokuro <keetasan7@gmail.com>
2022-10-03 23:49:10 +0900

    ➕ import sortパッケージを追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
-import React from "react";
 import Head from "next/head";
 import Image from "next/image";
+import React from "react";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
